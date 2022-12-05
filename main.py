c177f3bfad6408c3b6c6201d795d4ec007134537
Sayin <luffyp2001@gmail.com>
2022-12-05 15:27:15 -0500

    Templates

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request,make_response,redirect
+from flask import Flask, request,make_response,redirect, render_template
 
 app = Flask(__name__)
 
@@ -13,10 +13,10 @@ def index():
 
     return response
 
-    
+
 
 @app.route('/hello')
 
 def hello():
     user_ip = request.cookies.get('user_ip')
-    return f'Hello world Platzi, your IP is {user_ip}'
+    return render_template('hello.html', user_ip=user_ip )
