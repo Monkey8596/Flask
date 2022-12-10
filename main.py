f7f3026cfdb7b187653fc0ce70fb79fae0cff7dc
Sayin <luffyp2001@gmail.com>
2022-12-10 12:35:51 -0500

    LoginForm

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from flask import Flask, request,make_response,redirect, render_template, session
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -8,6 +12,11 @@ app.config['SECRET_KEY'] = 'SUPER_SECRET'
 
 
 ALL = ['Buy coffe', 'Send the request', 'Video Production']
+
+class LoginForm(FlaskForm):
+    username = StringField('UserName', validators=[DataRequired()] )
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Send')
 
 
 
@@ -34,8 +43,10 @@ def index():
 @app.route('/hello')
 def hello():
     user_ip = session.get('user_ip')
+    login_form = LoginForm()
     context = {
         'user_ip': user_ip,
          'ALL': ALL,
+         'login_form': login_form
     }
     return render_template('hello.html', **context)
---
+++ run_flask.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+pip install -r requirements.txt
+
+export FLASK_APP=main.py
+export FLASK_DEBUG=1
+export FLASK_ENV=development
+
+flask run
