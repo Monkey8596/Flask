0e39fbcfc8cb0c8d7d05300b9b751ed2acec72d4
Sayin <luffyp2001@gmail.com>
2022-12-12 21:20:57 -0500

    Test

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
+import unittest
 
 
 app = Flask(__name__)
@@ -18,6 +19,12 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Send')
 
+
+
+@app.cli.command()
+def test():
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner().run(tests)
 
 
 @app.errorhandler(404)
