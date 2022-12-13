197f16faa257a50ca0b9f75f7643263cd7cb4ed0
Sayin <luffyp2001@gmail.com>
2022-12-12 22:12:22 -0500

    ej3

--- GitHub.sh
+++ GitHub.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-
-# read -p "Dime tu nombre: " nombre
-# read -p "Mascota: " nombre_mascota
-# # Si queremos concatenar con guiones bajos, entonces:
-# mensaje="El correo de $nombre es ${nombre_mascota}_12@gmail.com"
-# echo $mensaje
-
 read -p "Commit name: " name
 read -p "Branch: " branch
 git add .
